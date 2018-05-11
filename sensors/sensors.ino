aa3896664dc3641d0b0bb6081d422c870ff7dae4
Leonard Eyer <le0nard@me.com>
2018-05-11 14:10:46 +0200

    Added struct. Starting communicating

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,5 +1,7 @@
 #include "DHT.h"
 
+#include <SoftwareSerial.h>
+
 //define sensor pins
 #define MOISTURE_SENSOR A0
 #define HUMIDITY_TEMPERATURE 2
@@ -22,12 +24,26 @@ SI114X radsens = SI114X();
 I2C_LCD LCD;
 uint8_t I2C_LCD_ADDRESS = 0x51; //Device address configuration, the default value is 0x51.
 
+typedef struct _data {
+  uint8_t flags;
+  uint8_t pid;
+  unsigned int timestamp;
+  uint8_t health;
+  uint8_t psize;
+  float temp;
+  float hum;
+  float rad; 
+}data;
+
+SoftwareSerial gtSerial(8, 7);
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(MOISTURE_SENSOR, INPUT);
   pinMode(HUMIDITY_TEMPERATURE, INPUT);
   
   Serial.begin(9600);
+  gtSerial.begin(9600);
   dht.begin();
   //start i2c communication
   Wire.begin();
@@ -45,10 +61,11 @@ void setup() {
 
   //Set the start coordinate.
   LCD.CharGotoXY(0,0);
+  /*
   LCD.println("Temp: ");
   LCD.println("Humidity: ");
   LCD.println("Moisture: ");
-  LCD.println("Radiation: ");
+  LCD.println("Radiation: ");*/
 }
 
 float temperature() {
@@ -59,9 +76,6 @@ float humidity() {
   return dht.readHumidity();
 }
 
-float moisture() {
-  return analogRead(MOISTURE_SENSOR)/1023.0;
-}
 
 float radiation() {
   return (radsens.ReadVisible() + radsens.ReadIR() + (radsens.ReadUV()/100.0))/3.0;
@@ -81,14 +95,36 @@ char* marshall () {
 
 
 void loop() {
+  data* it = malloc(sizeof(data));
   // put your main code here, to run repeatedly:
-  LCD.CharGotoXY(80,0);
+  // send data only when you receive data:
+  it->pid = 1;
+  it->health = 100;
+  it->psize = 5;
+  it->timestamp = millis();
+  it->temp = temperature();
+  it->hum = humidity();
+  it->rad = radiation();
+
+  int p = 15;
+
+  char* buf = malloc(sizeof(data) + 1);
+  memcpy(buf, it, sizeof(data));
+  buf[sizeof(data)] = '\0';
+
+  //Serial.write(buf, strlen(buf));
+  gtSerial.write("HEY");
+
+  /*Serial.write(it, sizeof(data));*/
+  /*LCD.CharGotoXY(80,0);
   LCD.print(temperature());
   LCD.CharGotoXY(80,16);
   LCD.print(humidity());
   LCD.CharGotoXY(80,32);
   LCD.print(moisture());
   LCD.CharGotoXY(80,48);
-  LCD.print(radiation());
+  LCD.print(radiation());*/
+
+  
 
 }
